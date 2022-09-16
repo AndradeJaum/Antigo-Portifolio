14e566d3b44ea956309b86c5bfa0f6868613764d
JoãoVitorAndrade <andradejaum@gmail.com>
2022-09-16 12:37:32 -0300

    test

--- src/screens/about/index.tsx
+++ src/screens/about/index.tsx
@@ -1,4 +1,5 @@
 import "./style.css"
+// import img from "../../images/me.png"
 
 function About() {
     return (
@@ -11,15 +12,15 @@ function About() {
                         <div className="about-line"></div>
                     </div>
                     <p className="about-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Non mollitia vero quae itaque, et cumque sequi consequatur quam facilis dolorem sit, a sunt cum. Provident molestias blanditiis sit. Sunt, alias!
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. At quia maiores, ratione eius ut optio sunt veritatis praesentium? Repellendus esse laborum quos! Sit omnis quam ipsum reprehenderit aperiam natus asperiores?
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. At quia maiores, ratione eius ut optio sunt veritatis praesentium? Repellendus esse laborum quos! Sit omnis quam ipsum reprehenderit aperiam natus asperiores?
                     </p>
                     <p className="about-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, animi corrupti! Excepturi dolore dolorem magnam quasi sequi ab veniam velit voluptate dolores exercitationem provident, odit temporibus odio aliquam pariatur ipsam.
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit pariatur a nesciunt nam odit unde hic eos saepe quasi excepturi quidem porro iste, quis, odio fugiat magni laboriosam, autem voluptatibus.
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit pariatur a nesciunt nam odit unde hic eos saepe quasi excepturi quidem porro iste, quis, odio fugiat magni laboriosam, autem voluptatibus.
                     </p>
 
                 </div>
                 <div className="about-image">
-                    <h2 className="about-tittle">Picture</h2>
+                    {/* <img src={img} className="image" ></img> */}
                 </div>
             </div>
         </>
--- src/screens/skills/index.tsx
+++ src/screens/skills/index.tsx
@@ -1,5 +1,4 @@
 import "./style.css"
-import TypewriterComponent from "typewriter-effect";
 
 function Skiils() {
     return (
@@ -8,7 +7,7 @@ function Skiils() {
                 <div className="skills-box">
                     <div className="skills-box-header">
                         <div className="skills-line"></div>
-                        <h2 className="skills-tittle">Skills and Experience</h2>
+                        <h2 className="skills-tittle">Skills and Experiences</h2>
                         <div className="skills-line"></div>
                     </div>
                     <p className="skills-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Non mollitia vero quae itaque, et cumque sequi consequatur quam facilis dolorem sit, a sunt cum. Provident molestias blanditiis sit. Sunt, alias!</p>
@@ -16,13 +15,20 @@ function Skiils() {
                     <p className="skills-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Non mollitia vero quae itaque, et cumque sequi consequatur quam facilis dolorem sit, a sunt cum. Provident molestias blanditiis sit. Sunt, alias!</p>
                 </div>
 
-                <div className="skills-image">
-                    <p>Javascript</p>
-                    <p>React.js</p>
-                    <p>Typescript</p>
-                    <p>Mongo</p>
-                    <p>AWS</p>
-                    <p>Node.js</p>
+                <div className="skills-tech">
+                    <div className="skills-tech-header">
+                        <h3 className="skills-subtittle">Technologies I study</h3>
+                    </div>
+                    <ul className="skills-list">
+                        <li className="skills-itens"><a className="techs">Javascript (ES6+)</a></li>
+                        <li className="skills-itens"><a className="techs">React.js</a></li>
+                        <li className="skills-itens"><a className="techs">Typescript</a></li>
+                        <li className="skills-itens"><a className="techs">WordPress</a></li>
+                        <li className="skills-itens"><a className="techs">Mongo</a></li>
+                        <li className="skills-itens"><a className="techs">AWS</a></li>
+                        <li className="skills-itens"><a className="techs">Node.js</a></li>
+                    </ul>
+
                 </div>
             </div>
         </>
