16f6a9f52c2ba46866719d23e62693e84a2adac6
AndradeJaum <andradejaum@gmail.com>
2022-10-28 12:42:35 -0300

    cssFix

--- src/pages/screens/about/index.tsx
+++ src/pages/screens/about/index.tsx
@@ -1,27 +1,26 @@
-import {i18next} from "../../../components/translate/i18n"
-import { useEffect } from "react";
+import { i18next } from "../../../components/translate/i18n"
 import HeaderModal from "../../../components/headerModal";
 import "./style.css"
 
 function About() {
 
-    return (
-        <>
-            <div className="about">
-                    <div className="about-box">
-                        <div className="about-box-header">
-                            <div className="about-line"></div>
-                            <h2 className="about-tittle">{i18next.t("tittles.about")}</h2>
-                            <div className="about-line"></div>
-                        </div>
-                        <p className="about-text">{i18next.t("messages.about1")} <br />
-                            <br />{i18next.t("messages.about2")} </p>
-                    </div>
-                    <div className="about-image">
-                    </div>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div className="about">
+        <div className="about-box">
+          <div className="about-box-header">
+            <div className="about-line"></div>
+            <h2 className="about-tittle">{i18next.t("tittles.about")}</h2>
+            <div className="about-line"></div>
+          </div>
+          <p className="about-text">{i18next.t("messages.about1")} <br />
+            <br />{i18next.t("messages.about2")} </p>
+        </div>
+        <div className="about-image">
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default About;
