eadc7dead73c36331b7d401e581f116bb0a69c62
AndradeJaum <andradejaum@gmail.com>
2022-10-31 11:42:45 -0300

    RefatoringWorks

--- src/pages/screens/skills/index.tsx
+++ src/pages/screens/skills/index.tsx
@@ -2,46 +2,46 @@ import { i18next } from "../../../components/translate/i18n"
 import "./style.css"
 
 function Skiils() {
-    return (
-        <>
-            <div className="skills">
-                <div className="skills-box">
-                    <div className="skills-box-header">
-                        <div className="skills-line"></div>
-                        <h2 className="skills-tittle">{i18next.t("tittles.skills")}</h2>
-                        <div className="skills-line"></div>
-                    </div>
-                    <p className="skills-text">{i18next.t("messages.skiils1")}</p>
-                    <p className="skills-text">{i18next.t("messages.skills2")}<strong><a href="http://statusgg.s3-website-us-east-1.amazonaws.com" target="_blank" className="skills-link">Status.gg</a></strong>, {i18next.t("messages.skills3")} <strong><a href="https://github.com/AndradeJaum/STATUSGG-Back-End" target="_blank" className="skills-link">BackEnd</a></strong> {i18next.t("messages.skills4")} <strong><a href="https://github.com/AndradeJaum/STATUSGG-Front-End" target="_blank" className="skills-link">FrontEnd</a></strong> {i18next.t("messages.skills5")} </p>
-                </div>
+  return (
+    <>
+      <div className="skills">
+        <div className="skills-box">
+          <div className="skills-box-header">
+            <div className="skills-line"></div>
+            <h2 className="skills-tittle">{i18next.t("tittles.skills")}</h2>
+            <div className="skills-line"></div>
+          </div>
+          <p className="skills-text">{i18next.t("messages.skiils1")}</p>
+          <p className="skills-text">{i18next.t("messages.skills2")}<strong><a href="http://statusgg.s3-website-us-east-1.amazonaws.com" target="_blank" className="skills-link">Status.gg</a></strong>, {i18next.t("messages.skills3")} <strong><a href="https://github.com/AndradeJaum/STATUSGG-Back-End" target="_blank" className="skills-link">BackEnd</a></strong> {i18next.t("messages.skills4")} <strong><a href="https://github.com/AndradeJaum/STATUSGG-Front-End" target="_blank" className="skills-link">FrontEnd</a></strong> {i18next.t("messages.skills5")} </p>
+        </div>
 
-                <div className="skills-tech">
-                    <div className="skills-tech-header">
-                        <div className="skills-line"></div>
-                        <h3 className="skills-subtittle">{i18next.t("tittles.tech")}</h3>
-                        <div className="skills-line"></div>
-                    </div>
-                    <div className="skills-list-box">
-                        <ul className="skills-list">
-                            <li className="skills-itens"><a className="techs">Javascript(ES6+)</a></li>
-                            <li className="skills-itens"><a className="techs">React.js</a></li>
-                            <li className="skills-itens"><a className="techs">Typescript</a></li>
-                            <li className="skills-itens"><a className="techs">HTML5</a></li>
-                            <li className="skills-itens"><a className="techs">CSS3</a></li>
-                            <li className="skills-itens"><a className="techs">WordPress</a></li>
-                        </ul>
-                        <ul className="skills-list">
-                            <li className="skills-itens"><a className="techs">Express.js</a></li>
-                            <li className="skills-itens"><a className="techs">Material-UI</a></li>
-                            <li className="skills-itens"><a className="techs">Mongo DB</a></li>
-                            <li className="skills-itens"><a className="techs">AWS</a></li>
-                            <li className="skills-itens"><a className="techs">Node.js</a></li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </>
-    )
+        <div className="skills-tech">
+          <div className="skills-tech-header">
+            <div className="skills-line"></div>
+            <h3 className="skills-subtittle">{i18next.t("tittles.tech")}</h3>
+            <div className="skills-line"></div>
+          </div>
+          <div className="skills-list-box">
+            <ul className="skills-list">
+              <li className="skills-itens"><a className="techs">Javascript(ES6+)</a></li>
+              <li className="skills-itens"><a className="techs">React.js</a></li>
+              <li className="skills-itens"><a className="techs">Typescript</a></li>
+              <li className="skills-itens"><a className="techs">HTML5</a></li>
+              <li className="skills-itens"><a className="techs">CSS3</a></li>
+              <li className="skills-itens"><a className="techs">WordPress</a></li>
+            </ul>
+            <ul className="skills-list">
+              <li className="skills-itens"><a className="techs">Express.js</a></li>
+              <li className="skills-itens"><a className="techs">Material-UI</a></li>
+              <li className="skills-itens"><a className="techs">Mongo DB</a></li>
+              <li className="skills-itens"><a className="techs">AWS</a></li>
+              <li className="skills-itens"><a className="techs">Node.js</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default Skiils;
--- src/pages/screens/work/index.tsx
+++ src/pages/screens/work/index.tsx
@@ -2,61 +2,61 @@ import { i18next } from "../../../components/translate/i18n"
 import "./style.css"
 import imgstatus from "../../../images/statusgg.png"
 import imgsearchmovie from "../../../images/searchmovie.png"
-import {} from "../../../components/translate/i18n"
+import { } from "../../../components/translate/i18n"
 
 function Work() {
 
-    
 
-    return (
-        <>
-            <div className="work">
-                <div className="work-box">
-                    <div className="work-box-header">
-                        <div className="work-line"></div>
-                        <h2 className="work-tittle">{i18next.t("tittles.works")}</h2>
-                        <div className="work-line"></div>
-                    </div>
-                    <div className="card-box">
-                        <a href="http://statusgg.s3-website-us-east-1.amazonaws.com" target="_blank" className="cards">
-                            <img src={imgstatus} className="images" />
-                            <div className="works-hidden">
-                                <h2 className="works-names">{i18next.t("works.statusName")}</h2>
-                                <p className="works-text">
-                                {i18next.t("works.statusText")}
-                                </p>
-                            </div>
-                        </a>
-                        <a href="http://searchmovie.s3-website-us-east-1.amazonaws.com" target="_blank" className="cards">
-                            <img src={imgsearchmovie} className="images" />
-                            <div className="works-hidden">
-                                <h2 className="works-names">{i18next.t("works.searchName")}</h2>
-                                <p className="works-text">
-                                {i18next.t("works.searchText")}
-                                </p>
-                            </div>
-                        </a>
-                        <a className="cards">
-                        <div className="works-hidden">
-                                <h2 className="works-names">{i18next.t("works.comingsoon")}</h2>
-                                <p className="works-text">
-                                {i18next.t("works.comingsoonText")}
-                                </p>
-                            </div>
-                        </a>
-                        <a className="cards">
-                        <div className="works-hidden">
-                                <h2 className="works-names">{i18next.t("works.comingsoon")}</h2>
-                                <p className="works-text">
-                                {i18next.t("works.comingsoonText")}
-                                </p>
-                            </div>
-                        </a>
-                    </div>
-                </div >
-            </div>
-        </>
-    )
+
+  return (
+    <>
+      <div className="work">
+        <div className="work-box">
+          <div className="work-box-header">
+            <div className="work-line"></div>
+            <h2 className="work-tittle">{i18next.t("tittles.works")}</h2>
+            <div className="work-line"></div>
+          </div>
+          <div className="card-box">
+            <a href="http://statusgg.s3-website-us-east-1.amazonaws.com" target="_blank" className="cards">
+              <img src={imgstatus} className="images" />
+              <div className="works-hidden">
+                <h2 className="works-names">{i18next.t("works.statusName")}</h2>
+                <p className="works-text">
+                  {i18next.t("works.statusText")}
+                </p>
+              </div>
+            </a>
+            <a href="http://searchmovie.s3-website-us-east-1.amazonaws.com" target="_blank" className="cards">
+              <img src={imgsearchmovie} className="images" />
+              <div className="works-hidden">
+                <h2 className="works-names">{i18next.t("works.searchName")}</h2>
+                <p className="works-text">
+                  {i18next.t("works.searchText")}
+                </p>
+              </div>
+            </a>
+            <a className="cards">
+              <div className="works-hidden">
+                <h2 className="works-names">{i18next.t("works.comingsoon")}</h2>
+                <p className="works-text">
+                  {i18next.t("works.comingsoonText")}
+                </p>
+              </div>
+            </a>
+            <a className="cards">
+              <div className="works-hidden">
+                <h2 className="works-names">{i18next.t("works.comingsoon")}</h2>
+                <p className="works-text">
+                  {i18next.t("works.comingsoonText")}
+                </p>
+              </div>
+            </a>
+          </div>
+        </div >
+      </div>
+    </>
+  )
 }
 
 export default Work;
